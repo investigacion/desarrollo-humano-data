247a26c1fe72a39801bd87cba373e1122ac08f18
Matthew Caruana Galizia <mattcg@gmail.com>
2013-09-30 17:17:04 -0600

    Hierarchical

--- scripts/csv2json.js
+++ scripts/csv2json.js
@@ -8,12 +8,26 @@ var csv = require('dsv').csv;
 console.log(JSON.stringify(require('dsv').csv.parse(require('fs').readFileSync(process.argv[2], {
 	encoding: 'utf8'
 })).reduce(function(subdivisions, row) {
-	var code;
+	var code, index, year;
 
 	code = row['Cantón'];
 	delete row['Cantón'];
 
-	subdivisions[code] = row;
+	if (!subdivisions[code]) {
+		subdivisions[code] = {};
+	}
+
+	index = row['Índice'].match(/\(([A-Za-z]+)\)/)[1];
+	delete row['Índice'];
+
+	year = row['Año'];
+	delete row['Año'];
+
+	if (!subdivisions[code][index]) {
+		subdivisions[code][index] = {};
+	}
+
+	subdivisions[code][index][year] = row;
 
 	return subdivisions;
 }, {}), null, '\t'));
